30735421d93b833475a0be641d29765501ebdfcf
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-26 04:19:23 +0300

    feat: diplay the new project or empty project components conditionally.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import ProjectsSidebar from "./components/Sidebar"
 import NoProjectSelected from "./components/noProjectSelected"
 import NewProject from "./components/newProject"
+import { useState } from "react"
 function App() {
+  const [projects,setProjects] = useState({
+    selectedProjectId : undefined,//if we are not adding project
+    allProjects:[]
+  })
+
+  const handleAddingProject = ()=>{
+    setProjects(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId:null,//if we want to add project
+      }
+    })
+  }
+  let content;
+  if(projects.selectedProjectId === null){
+    content = <NewProject/>
+  }else if(projects.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddingProject = {handleAddingProject}/>
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
+      <ProjectsSidebar onStartAddingProject = {handleAddingProject}/>
+      {content}
     </main>
 
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button"
-export default function ProjectsSidebar(){
+export default function ProjectsSidebar({onStartAddingProject}){
     return(
         <aside className="w-1/3 bg-stone-900 py-8 px-16 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8  font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
             <div>
-                <Button>
+                <Button onClick={onStartAddingProject}>
                     + Add Project
                 </Button>
             </div>
