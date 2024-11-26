9ab847efa0a67431dda359a58aa4175317013704
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-26 03:08:19 +0300

    feat: add sidebar for creating new projects

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ProjectsSidebar from "./components/Sidebar"
+import NewProject from "./components/newProject"
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar/>
+      
+    </main>
+
+  )
+
+
+}
+
+export default App
