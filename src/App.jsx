a45920b3a3c236eecb03c4903b8d9ffb86525f9d
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-27 03:43:59 +0300

    feat: diplay the newly added project on the sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,42 @@ import NoProjectSelected from "./components/noProjectSelected"
 import NewProject from "./components/newProject"
 import { useState } from "react"
 function App() {
-  const [projects,setProjects] = useState({
-    selectedProjectId : undefined,//if we are not adding project
-    allProjects:[]
+  const [projects, setProjects] = useState({
+    selectedProjectId: undefined,//if we are not adding project
+    allProjects: []
   })
 
-  const handleAddingProject = ()=>{
+  const handleAddingProject = () => {
     setProjects(prevState => {
-      return{
+      return {
         ...prevState,
-        selectedProjectId:null,//if we want to add project
+        selectedProjectId: null,//if we want to add project
       }
     })
   }
-  const handleNewProject = (projectData)=>{
+  const handleNewProject = (projectData) => {
     setProjects(prev => {
       const newProject = {
         ...projectData,
         id: Math.random()
       }
-      return{
-        ...prev,allProjects:[...prev.allProjects,newProject]
+      return {
+        ...prev,
+        selectedProjectId:undefined,
+         allProjects: [...prev.allProjects, newProject]
       }
     })
   }
-  
+
   let content;
-  if(projects.selectedProjectId === null){
-    content = <NewProject onNewProject={handleNewProject}/>
-  }else if(projects.selectedProjectId === undefined){
-    content = <NoProjectSelected onStartAddingProject = {handleAddingProject}/>
+  if (projects.selectedProjectId === null) {
+    content = <NewProject onNewProject={handleNewProject} />
+  } else if (projects.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddingProject={handleAddingProject} />
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddingProject = {handleAddingProject}/>
+      <ProjectsSidebar onStartAddingProject={handleAddingProject} projects={projects.allProjects} />
       {content}
     </main>
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button"
-export default function ProjectsSidebar({onStartAddingProject}){
-    return(
+export default function ProjectsSidebar({ onStartAddingProject, projects }) {
+    return (
         <aside className="w-1/3 bg-stone-900 py-8 px-16 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8  font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
             <div>
@@ -8,7 +8,12 @@ export default function ProjectsSidebar({onStartAddingProject}){
                     + Add Project
                 </Button>
             </div>
-            <ul></ul>
+            <ul className="mt-8">
+                {projects.map(project =>
+                    <li key={project.id}>
+                        <button className="w-full text-left px-2 py-rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
+                    </li>)}
+            </ul>
         </aside>
     )
 }
