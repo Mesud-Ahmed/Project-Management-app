070e9e3bd3320420f29d9b155acbf562e29c7382
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-27 11:37:22 +0300

    feat: ignore empty task values from being added.

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 export default function NewTask({onAdd}) {
-    const [enteredTask, setEnteredTask] = useState()
+    const [enteredTask, setEnteredTask] = useState('')
     const handleChange = (e) => {
         setEnteredTask(e.target.value)
     }
 
     const handleClick = ()=>{
+        if(enteredTask.trim()==''){
+            return
+        }
         onAdd(enteredTask)
         setEnteredTask('')
     }
